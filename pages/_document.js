f5530c2f58198b6d35d507306eb121edeb38fd94
Abhishek Jakhar <abhishekjakhar.11@gmail.com>
2021-07-17 20:31:35 +0530

    add favicons

--- pages/_document.js
+++ pages/_document.js
@@ -34,6 +34,26 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link href="/favicons/favicon.ico" rel="shortcut icon" />
+          <link href="/favicons/site.webmanifest" rel="manifest" />
+          <link
+            href="/favicons/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="180x180"
+          />
+          <link
+            href="/favicons/favicon-32x32.png"
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            href="/favicons/favicon-16x16.png"
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+          />
+
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
